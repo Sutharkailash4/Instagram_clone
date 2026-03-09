6e8995ff832ffa2f5a231d4f6eab147030f54c29
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-09 21:19:15 +0530

    fisrt commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const app = require("./src/app");
 const database = require("./src/config/database");
-const port = 3000;
+const Port = 3000;
 
 database();
 
-app.listen(port,()=>{
+app.listen(Port,()=>{
     console.log("Server is running on port 3000");
 })
