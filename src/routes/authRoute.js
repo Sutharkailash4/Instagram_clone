acb42a94a01db67166bf193972815c3c41203013
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-10 11:14:49 +0530

    changes

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,13 +3,16 @@ const userAuthentication = express.Router();
 const userModel = require(".././models/userModel");
 const jwt = require("jsonwebtoken");
 
-userAuthentication.post("/register",async(req,res)=>{
+userAuthentication.post("./register",async(req,res)=>{
     try{
         const data = req.body;
-        if(!data.name || data.name.trim()==="") return res
+        if(!data.name || data.name.trim()==="") return res.status(409).json({message : "Username is Required"});
+        else if(!data.email || data.email.trim()==="") return res.status(409).json({message : "Email is Required"});
+        else if(!data.password || data.password.trim()==="") return res.status(409).json({message : "Password is Required"});
+        
     }catch(error){
         res.status(400).json({
-            message : "Soemthing Went Wrong"
+            message : "Something Went Wrong"
         })
     }
 })
