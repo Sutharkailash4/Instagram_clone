1247570f83b8d87c203fbe7022456e5b12e90286
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-10 14:31:21 +0530

    commit

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userAuthentication = express.Router();
 const userModel = require(".././models/userModel");
 const jwt = require("jsonwebtoken");
+const crypto = required("crypto");
 
 userAuthentication.post("./register",async(req,res)=>{
     try{
@@ -23,7 +24,31 @@ userAuthentication.post("./register",async(req,res)=>{
             //         message : "User With This Email is Already Exists"
             //     })
             // }
+            const isUserAlreadyExists = await userModel.findOne({
+                $or : [
+                    {name},
+                    {email}
+                ]
+            })
+            if(isUserAlreadyExists){
+                return res.status(409).json({
+                    message : "User Already Exists"
+                })
+            }
+            const hash_password = crypto.createHash("md5").update(password).digest("hex");
+            const user = await userModel.create({
+                name : name,
+                email : email,
+                password : hash_password
+            })
+            const token = jwt.sign(
+            {
+                    id : user._id,
+                    email : user.email
+                
+            },
             
+        )
         }
     }catch(error){
         res.status(400).json({
