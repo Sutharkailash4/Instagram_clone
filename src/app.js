270b83303150b3d4a5c4092a966dead3f452920c
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-11 12:43:44 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const app = express();
+const userModel = require("./models/userModel");
 const userAuth = require("./routes/authRoute");
 const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use("/api/auth",userAuth);
 app.use(cookieParser());
+
+app.get("/users",async(req,res)=>{
+    try{
+        const allUser = await userModel.find();
+        if(!find){
+            return res.status(409).json({
+                message : "User Not Found"
+            })
+        }
+    }catch(error){
+        res.status(400).json({
+            message : "Something Went Wrong"
+        })
+    }
+})
+
+app.delete("/remove/:id",async(req,res)=>{
+    try{    
+        const id = req.params.id;
+        await userModel.findByIdAndDelete(id);
+        res.status(200).json({
+            message : "User Deleted Successfully"
+        })
+    }catch(error){
+        res.status(400).json({
+            message : "Something Went Wrong"
+        })
+    }
+})
+
 module.exports = app;
