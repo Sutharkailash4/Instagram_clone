f64c4a6b7387a0aaf0cbe0c137b6a9cb2e962707
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-18 10:18:20 +0530

    changes

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -5,8 +5,10 @@ async function postCreateController(req,res){
        res.status(200).json({
         message : "Post Created Successfully",
         post : req.body,
-        file : req.file 
+        file : req.file
        })
+       console.log(req.body);
+       console.log(req.file);
     }catch(error){
         res.status(400).json({
             message : "Something Went Wrong"
