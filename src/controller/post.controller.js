ac840293597991348922701353928c2ce0a32819
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-18 10:49:16 +0530

    multer

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -2,13 +2,15 @@ const model = require(".././models/post.model");
 
 async function postCreateController(req,res){
     try{
-       res.status(200).json({
+        console.log(req.body);
+        console.log(req.file);
+
+        res.status(200).json({
         message : "Post Created Successfully",
         post : req.body,
         file : req.file
        })
-       console.log(req.body);
-       console.log(req.file);
+     
     }catch(error){
         res.status(400).json({
             message : "Something Went Wrong"
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const userPostRoute = express.Router();
 const postControllers = require(".././controller/post.controller");
+const multer = require("multer");
+const upload = multer({
+    storage : multer.memoryStorage()
+});
 
-userPostRoute.post("/userpost",postControllers.postCreateController);
+userPostRoute.post("/userpost",upload.single("imageURL"),postControllers.postCreateController);
 
 module.exports = userPostRoute;
