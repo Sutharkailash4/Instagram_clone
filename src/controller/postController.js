0e43d38b76a2a17cb3205b49ab623afc05d4db3c
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-23 14:35:48 +0530

    changes

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -1,11 +1,21 @@
 const model = require(".././models/postModel");
 const ImageKit = require("@imageKit/nodejs");
+const client = new ImgeKit({
+    private_key : process.env.IMAGEKIT_PRIVATE_KEY
+});
 
 const createPostController = async (req,res) => {
     try{
-        console.log(req.body);
-        console.log(req.file.originalname);
-        res.send("Ok");
+        const uploadToImagekit = await client.files.upload({
+            file : req.file,
+            filename : req.file.originalname
+        });
+        res.status(201).json({
+            message : "Post Created Successfully",
+            caption : req.body.caption,
+            post_image : req.file,
+            uploadToImageKit = uploadImageKit
+        })
     }catch(error){
         res.status(400).json({
             message : "Something Went Wrong",
