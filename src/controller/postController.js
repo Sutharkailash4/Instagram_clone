329bcba2fe8c3a92dff93428645518cf7eb314de
kailash suthar <kailashsuthar4488@gmail.com>
2026-04-01 13:29:29 +0530

    add middleware

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -9,16 +9,6 @@ const client = new ImageKit({
 const createPostController = async (req,res) => {
     try{
         if(!req.file) return res.status(409).json({message : ""})
-        
-        const token = req.cookies.access_token;
-
-        if(!token){
-            return res.status(409).json({
-                message : "Token Not Provided ! Unauthorized Access"
-            })
-        }
-
-        const user = jwt.verify(token, process.env.JWT_ACCESS_TOKEN);
 
         const uploadToImagekit = await client.files.upload({
             file : await toFile(Buffer.from(req.file.buffer),"post_image"),
@@ -29,7 +19,7 @@ const createPostController = async (req,res) => {
         const userPost = await model.create({
             caption : req.body.caption,
             post_image : uploadToImagekit.url,
-            user : user.id
+            user : req.user.id
         })
 
         res.status(201).json({
@@ -48,14 +38,8 @@ const createPostController = async (req,res) => {
 
 const getPostController = async (req,res) => {
    try{
-       const token = req.cookies.access_token;
-       if(!token){
-        return res.status(400).json({
-            message : "Cookie not Provided ! Unauthorized Access"
-        })
-       }
-       const user = jwt.verify(token, process.env.JWT_ACCESS_TOKEN);
-       const user_id = user.id;
+      
+       const user_id = req.user.id;
        const posts = await model.find({
         user : user_id
        })
@@ -75,7 +59,6 @@ const getPostController = async (req,res) => {
 const getPostDetailsController = async (req,res) => {
     try{
        const post_id = req.params.postId;
-       const token = req.cookies.access_token;
 
        const isPostExists = await model.find({
             _id : post_id
@@ -83,9 +66,7 @@ const getPostDetailsController = async (req,res) => {
 
        if(isPostExists.length===0) return res.status(400).json({message : "Post Not Availabe on This Id"});
        
-      const user = jwt.verify(token ,process.env.JWT_ACCESS_TOKEN);
-
-      if(isPostExists[0].user.toString() !== user.id) return res.status(409).json({message : "Unauthorized Access ! Please Try Again"});
+      if(isPostExists[0].user.toString() !== req.user.id) return res.status(409).json({message : "Unauthorized Access ! Please Try Again"});
 
       res.status(200).json({
         message : "Post Details Feched Successfully",
