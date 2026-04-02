a93bafad99f8bb6067dc5e8a7b4a0f9d66690a1d
kailash suthar <kailashsuthar4488@gmail.com>
2026-04-02 10:13:26 +0530

    chnages

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const ImageKit = require("@imagekit/nodejs");
+const { toFile } = require("@imagekit/nodejs");
 const client = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
 });
@@ -14,12 +15,16 @@ const createPostController = async (req, res) => {
         const token = req.cookies.access_token;
         if (!token) {
             return res.status(400).json({
-                message : "Token Not Provided ! Unauthorized Access"
+                message: "Token Not Provided ! Unauthorized Access"
             })
         }
         const user = jwt.verify(token, process.env.JWT_ACCESS_TOKEN);
-        
-        res.send(req.file);
+        const response = await client.files.upload({
+            file: file.buffer.toString("base64"),
+            fileName: file.originalname,
+            folder: "Instgarm-clone-posts"
+        });
+        res.send("ok");
     } catch (error) {
         res.status(400).json({
             message: "Something Went Wrong",
