d0bebdc036897f9011f6410db632ca2ee439149e
kailash suthar <kailashsuthar4488@gmail.com>
2026-04-03 10:56:29 +0530

    post like and unlike

--- src/models/likeModel.js
+++ src/models/likeModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mognoose");
+const mongoose = require("mongoose");
 
 const likeSchema = new mongoose.Schema({
     user: {
--- src/routes/likeRoutes.js
+++ src/routes/likeRoutes.js
@@ -4,6 +4,6 @@ const authenticationMiddleware = require(".././middleware/authMiddleware");
 const likeControllers = require(".././controllers/likeController");
 
 likeRoute.post("/like/:postId", authenticationMiddleware, likeControllers.likeUserController);
-likeRoute.post("unLike/:postId", authenticationMiddleware, likeControllers.unLikeUserController);
+likeRoute.post("/unLike/:postId", authenticationMiddleware, likeControllers.unLikeUserController);
 
 module.exports = likeRoute;
